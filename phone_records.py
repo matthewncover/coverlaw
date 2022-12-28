95f54b9d52405d2e8a781f1b390331e5fe3acdac
Matthew Cover <41007986+matthewncover@users.noreply.github.com>
2022-12-28 15:16:15 -0800

    merge data, remove duplicates

--- phone_records.py
+++ phone_records.py
@@ -1,8 +1,13 @@
 import os, pandas as pd
 
+import warnings
+warnings.simplefilter("ignore")
+
 class PhoneRecords:
 
     def __init__(self, file_root):
+        """
+        """
 
         self.file_root = file_root
 
@@ -12,12 +17,19 @@ class PhoneRecords:
 
     
     def preprocess_files(self):
+        """
+        """
 
         self.__preprocess_contacts_file()
         self.__preprocess_verizon_files()
         self.__preprocess_client_files()
+
+        self.log = self.df_verizon.merge(self.df_contacts, on="Number", how="left")
     
+
     def __preprocess_contacts_file(self):
+        """
+        """
 
         df_contacts = pd.read_excel(os.path.join(self.file_root, self.contacts_filename))
         df_contacts.columns = [str.capitalize(x) for x in df_contacts.columns]
@@ -25,9 +37,21 @@ class PhoneRecords:
         df_contacts.Number = df_contacts.Number.map(lambda x: pd.NA if pd.isna(x) else str(int(x)))
         df_contacts = df_contacts.replace({" ": pd.NA}).dropna(how="all", axis=1)
 
+        # keeps names listed first for all duplicate numbers.
+        # EDA demonstrated that the names were similar enough to get away with that
+        df_contacts = (
+            df_contacts.loc[
+                df_contacts.Number
+                .drop_duplicates(keep="first").index
+                ].reset_index(drop=True)
+            )
+
         self.df_contacts = df_contacts
 
+
     def __preprocess_verizon_billed(self):
+        """
+        """
 
         df_verizonb = pd.read_csv(os.path.join(self.file_root, self.verizon_billed_filename), skiprows=13)
 
@@ -53,10 +77,14 @@ class PhoneRecords:
         )
 
         df_verizonb["billed"] = True
+        df_verizonb.Number = df_verizonb.Number.map(lambda x: pd.NA if pd.isna(x) else str(int(x)))
 
         self.df_verizonb = df_verizonb
 
+
     def __preprocess_verizon_unbilled(self):
+        """
+        """
 
         df_verizonu = pd.read_excel(os.path.join(self.file_root, self.verizon_unbilled_filename), skiprows=3)
 
@@ -68,10 +96,14 @@ class PhoneRecords:
         )
 
         df_verizonu["billed"] = False
+        df_verizonu.Number = df_verizonu.Number.map(lambda x: pd.NA if pd.isna(x) else str(int(x)))
 
         self.df_verizonu = df_verizonu
 
+
     def __preprocess_verizon_files(self):
+        """
+        """
 
         if self.verizon_billed_filename and self.verizon_unbilled_filename:
 
@@ -92,7 +124,10 @@ class PhoneRecords:
         else:
             assert False, "No verizon file listed"
 
+
     def __preprocess_client_files(self):
+        """
+        """
 
         client_filenames = [x for x in os.listdir(self.file_root) if x.startswith("_")]
 
@@ -101,7 +136,10 @@ class PhoneRecords:
 
         self.df_client = df_client
 
+    
     def __read_clean_client_file(self, x):
+        """
+        """
 
         df_client = pd.read_excel(os.path.join(self.file_root, x), header=None)
 
@@ -131,8 +169,10 @@ class PhoneRecords:
             return x.strip("_").replace(".xlsx", "")
 
         df_client["client name"] = strip_client_name(x)
+        df_client.Number = df_client.Number.map(lambda x: pd.NA if pd.isna(x) else str(int(x)))
 
         return df_client
+
 
 if __name__ == "__main__":
 
